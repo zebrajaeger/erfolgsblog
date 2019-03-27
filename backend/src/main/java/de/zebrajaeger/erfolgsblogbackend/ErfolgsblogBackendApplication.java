2a82e24a9e514900b1ad3482115476879b6c8551
Lars Brandt <brandt@silpion.de>
2019-03-27 11:28:21 +0100

    initial

---
+++ backend/src/main/java/de/zebrajaeger/erfolgsblogbackend/ErfolgsblogBackendApplication.java
@@ -0,0 +1,13 @@
+package de.zebrajaeger.erfolgsblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErfolgsblogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErfolgsblogBackendApplication.class, args);
+	}
+
+}
